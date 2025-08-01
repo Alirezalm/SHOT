17204338baecff106dedd14043b7aab8ae758e3e
Alireza <Alireza.Olama@abo.fi>
2025-08-01 17:21:53 +0300

    initial threadpool implementation

---
+++ src/Concurrency/SHOTQueue.h
@@ -0,0 +1,129 @@
+#pragma once
+
+#include <memory>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <atomic>
+#include <stdexcept>
+
+namespace SHOT
+{
+template <typename Task> class SHOTQueue
+{
+public:
+    SHOTQueue() : stop(false) { }
+
+    SHOTQueue(const SHOTQueue&) = delete;
+    SHOTQueue& operator=(const SHOTQueue&) = delete;
+
+    SHOTQueue(SHOTQueue&&) = delete;
+    SHOTQueue& operator=(SHOTQueue&&) = delete;
+
+    ~SHOTQueue() { shutdown(); };
+
+    bool tryPop(Task& task)
+    {
+
+        std::unique_lock<std::mutex> lock(mtx);
+
+        if(queue.empty())
+        {
+            return false;
+        }
+
+        task = std::move(queue.front());
+        queue.pop();
+        return true;
+    }
+
+    std::shared_ptr<Task> tryPop()
+    {
+        std::unique_lock<std::mutex> lock(mtx);
+
+        if(queue.empty())
+        {
+            return std::shared_ptr<Task>();
+        }
+
+        std::shared_ptr<Task> taskPtr = std::make_shared<Task>(std::move(queue.front()));
+        queue.pop();
+        return taskPtr;
+    }
+
+    bool waitAndPop(Task& task)
+    {
+        std::unique_lock<std::mutex> lock(mtx);
+
+        cv.wait(lock, [this]() -> bool { return !queue.empty() || stop; });
+
+        if(queue.empty())
+        {
+            return false;
+        }
+
+        task = std::move(queue.front());
+
+        queue.pop();
+
+        return true;
+    }
+
+    std::shared_ptr<Task> waitAndPop()
+    {
+        std::unique_lock<std::mutex> lock(mtx);
+
+        cv.wait(lock, [this]() -> bool { return !queue.empty() || stop; });
+
+        if(queue.empty())
+        {
+            return std::shared_ptr<Task>();
+        }
+
+        std::shared_ptr<Task> taskPtr = std::make_shared<Task>(std::move(queue.front()));
+
+        queue.pop();
+
+        return taskPtr;
+    }
+
+    void push(Task task)
+    {
+
+        std::unique_lock<std::mutex> lock(mtx);
+
+        if(stop)
+        {
+            throw std::runtime_error("Cannot push to a stopped queue");
+        }
+
+        queue.push(std::move(task));
+
+        cv.notify_one();
+    }
+
+    bool isStopped() const { return stop; }
+
+    void shutdown()
+    {
+
+        {
+            std::lock_guard<std::mutex> lock(mtx);
+            stop = true;
+        }
+
+        cv.notify_all();
+    }
+    bool isEmpty() const
+    {
+        std::lock_guard<std::mutex> lock(mtx);
+        return queue.empty();
+    }
+
+private:
+    std::queue<Task> queue;
+    mutable std::mutex mtx;
+    std::condition_variable cv;
+    std::atomic<bool> stop;
+};
+}
---
+++ src/Concurrency/SHOTThreadPool.h
@@ -0,0 +1,158 @@
+#pragma once
+
+#include <cstddef>
+#include <atomic>
+#include <memory>
+#include <thread>
+#include <type_traits>
+#include <vector>
+#include <functional>
+#include <future>
+#include "SHOTQueue.h"
+
+namespace SHOT
+{
+
+class IThreadTask
+{
+public:
+    IThreadTask(void) = default;
+    virtual ~IThreadTask(void) = default;
+    IThreadTask(const IThreadTask& rhs) = delete;
+    IThreadTask& operator=(const IThreadTask& rhs) = delete;
+    IThreadTask(IThreadTask&& other) = default;
+    IThreadTask& operator=(IThreadTask&& other) = default;
+    virtual void execute() = 0;
+};
+
+template <typename Func> class ThreadTask : public IThreadTask
+{
+public:
+    ThreadTask(Func&& func) : func(std::move(func)) { }
+    ~ThreadTask(void) override = default;
+
+    ThreadTask(const ThreadTask& rhs) = delete;
+    ThreadTask& operator=(const ThreadTask& rhs) = delete;
+
+    ThreadTask(ThreadTask&& other) = default;
+    ThreadTask& operator=(ThreadTask&& other) = default;
+
+    void execute() override { func(); }
+
+private:
+    Func func;
+};
+
+template <typename T> class SHOTFuture
+{
+public:
+    SHOTFuture(std::future<T>&& future) : future(std::move(future)) { };
+
+    SHOTFuture(const SHOTFuture& rhs) = delete;
+    SHOTFuture& operator=(const SHOTFuture& rhs) = delete;
+
+    SHOTFuture(SHOTFuture&& other) = default;
+    SHOTFuture& operator=(SHOTFuture&& other) = default;
+
+    ~SHOTFuture()
+    {
+        if(future.valid())
+        {
+            future.get();
+        }
+    }
+
+    auto get() { return future.get(); }
+
+private:
+    std::future<T> future;
+};
+
+class SHOTThreadPool
+{
+public:
+    SHOTThreadPool(const size_t numThreads) : done { false }, workQueue {}, threads {}
+    {
+
+        try
+        {
+            for(size_t i = 0; i < numThreads; ++i)
+            {
+                threads.emplace_back(&SHOTThreadPool::workerThread, this);
+            }
+        }
+        catch(...)
+        {
+            shutdown();
+            throw;
+        }
+    };
+
+    SHOTThreadPool(const SHOTThreadPool& rhs) = delete;
+    SHOTThreadPool& operator=(const SHOTThreadPool& rhs) = delete;
+
+    SHOTThreadPool(SHOTThreadPool&& other) = delete;
+    SHOTThreadPool& operator=(SHOTThreadPool&& other) = delete;
+
+    ~SHOTThreadPool() { shutdown(); }
+
+    template <typename Func, typename... Args> auto submitTask(Func&& func, Args&&... args)
+    {
+        auto boundTask = std::bind(std::forward<Func>(func), std::forward<Args>(args)...);
+
+        using ResultType = std::invoke_result_t<decltype(boundTask)>;
+
+        using PackagedTask = std::packaged_task<ResultType()>;
+
+        using TaskType = ThreadTask<PackagedTask>;
+
+        PackagedTask task(std::move(boundTask));
+
+        SHOTFuture<ResultType> result(task.get_future());
+
+        workQueue.push(std::make_unique<TaskType>(std::move(task)));
+
+        return result;
+    }
+
+private:
+    void workerThread()
+    {
+
+        while(!done)
+        {
+
+            std::unique_ptr<IThreadTask> pTask;
+
+            if(workQueue.tryPop(pTask))
+            {
+
+                pTask->execute();
+            }
+            else
+            {
+                std::this_thread::yield();
+            }
+        }
+    }
+
+    void shutdown()
+    {
+        done = true;
+        workQueue.shutdown();
+        for(auto& thread : threads)
+        {
+            if(thread.joinable())
+            {
+                thread.join();
+            }
+        }
+        threads.clear();
+    }
+
+    std::atomic<bool> done;
+    SHOTQueue<std::unique_ptr<IThreadTask>> workQueue;
+    std::vector<std::thread> threads;
+};
+
+};
